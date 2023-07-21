bb0f49ff60fdc4089d2a33b8b2ba96e4401ab7aa
rohitpanchalll <panchalrohit0021@gmail.com>
2023-07-21 16:21:31 +0530

    fetched coinapi data

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -27,5 +31,20 @@ const List<String> cryptoList = [
   'ETH',
   'LTC',
 ];
+const coinApiUrl = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = 'CCECB822-D4B5-42E3-AC05-C71C671A503F';
 
-class CoinData {}
+class CoinData {
+  Future getCoinData() async {
+    String requestURL = "$coinApiUrl/BTC/USD?apikey=$apiKey";
+    http.Response response = await http.get(Uri.parse(requestURL));
+
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      var lastPrice = decodedData['rate'];
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
